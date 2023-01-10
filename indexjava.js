166933c3169e3917ef64a01135f9a3d46b257e11
Alberte Faghtmann <alberte_jubii@hotmail.com>
2023-01-10 13:18:11 +0100

    done

--- indexjava.js
+++ indexjava.js
@@ -9,4 +9,5 @@ function startAnimation() {
   console.log("startAnimation");
 
   document.querySelector("portfolio").classList.add("portfolio flyv");
+  document.querySelector("navn").classList.add("navn flyvnavn");
 }
